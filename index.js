83527448f0749337aa4aeb78ff90900796325273
vishalllxd <itzvishal739@gmail.com>
2026-06-21 06:42:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const http = require('http');
 
-// Keep-alive web server for Render
 http.createServer((req, res) => {
   res.writeHead(200);
   res.end('Bot is running!');
@@ -24,21 +23,17 @@ client.once('ready', () => {
 
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
-
   const args = message.content.trim().split(/\s+/);
   const command = args[0].toLowerCase();
-
   if (command === '?role') {
     if (!message.mentions.members.size) {
-      return message.reply('Please mention a member! Example: `?Role @SomeUser`');
+      return message.reply('Please mention a member! Example: ?Role @SomeUser');
     }
-
     const targetMember = message.mentions.members.first();
     const role = message.guild.roles.cache.find(r => r.name === ROLE_NAME);
-
     if (!role) {
-      return message.reply(`Could not find the role named "${ROLE_NAME}". Check the role name!`);
+      return message.reply('Could not find the role. Check the role name!');
     }
-
     if (targetMember.roles.cache.has(role.id)) {
-      await
+      await targetMember.roles.remove(role);
+      message.reply('Removed the ' + ROLE_NAME + ' role from ' +
